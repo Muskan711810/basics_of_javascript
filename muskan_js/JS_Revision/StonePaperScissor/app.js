b781c7a867fce92c085e5e4d84e6f19b92f78a21
Muskaan Shaikh <ymuskaanshaikh0440@gmail.com>
2024-08-24 01:52:17 +0530

    solved bugs

--- muskan_js/JS_Revision/StonePaperScissor/app.js
+++ muskan_js/JS_Revision/StonePaperScissor/app.js
@@ -2,6 +2,7 @@ let userscore = 0;
 let compscore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
 const gencompchoice = () =>{
     const options = ["stone","paper","scissor"];
@@ -11,6 +12,17 @@ const gencompchoice = () =>{
 
 const drawGame= () => {
     console.log("game was draw.");
+    msg.innerText("Game was Draw...Play Again")
+};
+
+const showWinner = (userwin) => {
+    if(userwin){
+        console.log("you win....");
+         msg.innerText = "You Win..."
+    }else {
+        console.log("you lose...");
+         msg.innerText = "You Lose..."
+    }
 };
 
 const playGame = (userchoice) => {
@@ -18,20 +30,27 @@ const playGame = (userchoice) => {
     //generate computer choice
     const compchoice = gencompchoice;
     console.log("comp choice", compchoice);
-};
 
-if (userscore === compscore){
+if (userchoice === compchoice){
     drawGame();
-}else{
+} else {
     let userwin = true;
     if(userchoice === "stone"){
         //paper scissors,
-        userwin = 
+        userwin = compchoice ==="paper" ? false : true;
+    } else if (userchoice ==="paper"){
+        //stone scissors
+        userwin = compchoice ==="scissor" ? false : true;
+    } else{
+        userwin = compchoice ==="stone" ?false : true;
     }
-}
+    showWinner();
+    }
+};
 
-choices.forEach ((choice)=> {
-    choice.addEventListner("click", () => {
+
+choices.forEach ((choice) => {
+    choice.addEventListener("click", () => {
         const userchoice = choice.getAttribute("id");
         playGame(userchoice);
     });
